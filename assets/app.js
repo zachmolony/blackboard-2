3678b09edd31549356373e26191bd51aef0a4248
zachmolony <31472522+zachmolony@users.noreply.github.com>
2018-10-29 16:54:13 +0000

    load notes into editor page

--- assets/app.js
+++ assets/app.js
@@ -9,4 +9,23 @@
             axios.post('/api/notes').then(response => (window.location = '/notes/'+response.data.data.Slug))
         },
 
+        // Load the notes to the page
+        loadNotes: evt => {
+            axios.get('/api/notes').then(response => {
+                const notes = response.data.data
+                for (const key in notes) {
+                    if (notes.hasOwnProperty(key)) {
+                        notesList.append(
+                            `<a href="/notes/${notes[key].Slug}" class="list-group-item list-group-item-action">
+                                ${notes[key].Title}
+                            </a>`
+                        )
+                    }
+                }
+            })
+        }
+    }
+
+    noteDocument.ready(helpers.loadNotes)
+    createButton.click(helpers.createNoteAndRedirect)
 }())
